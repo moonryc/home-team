27c200bb04c9a89bdf1cedaccbffd21f0a77f42a
moonryc <70920813+moonryc@users.noreply.github.com>
2022-03-10 15:48:46 -0600

    refactor and simplification, now the playerList is updated each time the user types in a letter, each time you type a letter it runs a fetch request.

--- app/src/main/java/com/example/hometeam/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/hometeam/viewmodels/MainViewModel.kt
@@ -3,62 +3,55 @@ package com.example.hometeam.viewmodels
 import android.text.Editable
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.hometeam.models.Player
 import com.example.hometeam.models.PlayerList
-import com.example.hometeam.models.UserInfo
 import com.google.gson.GsonBuilder
 import okhttp3.*
 import java.io.IOException
 
 
-class MainViewModel :ViewModel() {
+class MainViewModel : ViewModel() {
 
+    //Used to display the data on screen observed by the MainActivity
+    val playerList = MutableLiveData<PlayerList>()
+    //What the user is searching
     var search = ""
-    val user = MutableLiveData<UserInfo>()
-    val selectedPlayer = MutableLiveData<Player>()
 
 
     init {
-        user.value = UserInfo("Tony Stark", "https://bit.ly/2zpY4w4")
-        selectedPlayer.value = Player()
-
     }
 
+    /**
+     * Runs each time the user changes the text field, taking the searchedPlayer, paring to a string
+     * and formating any spaces into %20 for url queries
+     */
+    fun updateSearch(searchedPlayer: Editable?) {
 
+        search = searchedPlayer.toString().replace(" ", "%20")
 
-    fun updateSearch(searchedPlayer:Editable?){
-        search = searchedPlayer.toString()
-        println("the search was changed!")
+        if (search != "") {
+            val url = "https://www.thesportsdb.com/api/v1/json/50130162/searchplayers.php?p=$search"
+            val request = Request.Builder().url(url).build()
+            val client = OkHttpClient()
+            client.newCall(request).enqueue(object : Callback {
+
+                override fun onResponse(call: Call, response: Response) {
+                    val body = response.body?.string()
+                    val gson = GsonBuilder().create()
+
+                    val playerListResponse = gson.fromJson(body, PlayerList::class.java)
+
+                    playerList.postValue(playerListResponse)
+                }
+
+                override fun onFailure(call: Call, e: IOException) {
+                    println("============================")
+                    println("failed to execute request")
+                    println("============================")
+                }
+            })
+        }
     }
 
-
-
-    fun fetchPlayer(playerName:Editable?){
-        user.value = UserInfo(playerName.toString(), "https://bit.ly/2zpY4w4")
-        val url = "https://www.thesportsdb.com/api/v1/json/50130162/searchplayers.php?p=Danny"
-        val request = Request.Builder().url(url).build()
-        val client = OkHttpClient()
-        client.newCall(request).enqueue(object: Callback{
-
-            override fun onResponse(call: Call, response: Response) {
-                val body = response.body?.string()
-                val gson = GsonBuilder().create()
-
-                println("=========BODY=========")
-                println(body)
-
-                val playerList = gson.fromJson(body, PlayerList::class.java)
-
-            }
-
-            override fun onFailure(call: Call, e: IOException) {
-                println("============================")
-                println("failed to execute request")
-                println("============================")
-            }
-
-        })
-    }
 }
 
 
