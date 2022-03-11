8ad0deebe0c57e89f6f661bd0f5433265716c6d4
moonryc <70920813+moonryc@users.noreply.github.com>
2022-03-10 22:43:26 -0600

    prevented made it so that the first option for picture use is avalible

--- app/src/main/java/com/example/hometeam/models/Player.kt
+++ app/src/main/java/com/example/hometeam/models/Player.kt
@@ -1,5 +1,8 @@
 package com.example.hometeam.models
 
+import com.bumptech.glide.Glide
+import com.example.hometeam.R
+
 data class Player(
     var strPlayer: String = "",
     val strTeam: String = "",
@@ -13,7 +16,6 @@ data class Player(
     val strBirthLocation: String = "",
     val strDescriptionEN: String = "",
     val strPosition: String = "",
-    val strCollege: String = "",
     val strHeight: String = "",
     val strWeight: String = "",
     val strThumb: String = "",
@@ -21,4 +23,20 @@ data class Player(
     val strRender: String = "",
     val strBanner: String = "",
 
-    )
+    ){
+
+    fun getImage():String{
+        if(strThumb !== null){
+            return strThumb
+        }else if(strCutout !== null){
+            return strCutout
+        }
+        else if(strRender !== null){
+            return strRender
+        }
+        else{
+            return ""
+        }
+    }
+
+}
