1be924971728bc087fbf996701f07bfc1063bd1b
moonryc <70920813+moonryc@users.noreply.github.com>
2022-03-10 22:40:16 -0600

    the functionality for hiding and showing elements, fleshed out the onClick to make Player selection work. Added a bunch of get elements

--- app/src/main/java/com/example/hometeam/view/MainActivity.kt
+++ app/src/main/java/com/example/hometeam/view/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.hometeam.view
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -14,17 +13,20 @@ import com.example.hometeam.viewmodels.MainViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hometeam.R
-import com.example.hometeam.models.Player
 import com.example.hometeam.models.PlayerAdapter
-import com.example.hometeam.models.PlayerList
+import androidx.cardview.widget.CardView
 
 
-class MainActivity : AppCompatActivity(),PlayerAdapter.OnItemClickListener {
+class MainActivity : AppCompatActivity(), PlayerAdapter.OnItemClickListener {
 
     private var viewManager = LinearLayoutManager(this)
     lateinit var binding: ActivityMainBinding
     lateinit var viewModel: MainViewModel
-    lateinit var recycler_view:RecyclerView
+    lateinit var recycler_view: RecyclerView
+    lateinit var backButton: Button
+    lateinit var searchButton: Button
+    lateinit var searchField: EditText
+    lateinit var selectedPlayerView:CardView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,21 +43,22 @@ class MainActivity : AppCompatActivity(),PlayerAdapter.OnItemClickListener {
         binding.lifecycleOwner = this
 
 
+        //get Elements
         recycler_view = findViewById(R.id.recycler_view)
-        //        val COUNTRIES: Array<String> = arrayOf<String>("hello","asdf")
-//        val editText = findViewById(R.id.actv) as AutoCompleteTextView
-//        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,COUNTRIES)
-//        editText.setAdapter(adapter)
+        backButton = findViewById(R.id.backButton)
+        searchButton = findViewById(R.id.searchButton)
+        searchField = findViewById(R.id.searchField)
+        selectedPlayerView = findViewById(R.id.selected_player_layout)
 
-
-//        recycler_view.adapter = PlayerAdapter(viewModel.exampleList)
-//        recycler_view.layoutManager = LinearLayoutManager(this)
-//        recycler_view.setHasFixedSize(true)
 
         initialiseAdapter()
+
     }
 
-    private fun initialiseAdapter(){
+
+
+
+    private fun initialiseAdapter() {
         recycler_view.layoutManager = viewManager
         recycler_view.setHasFixedSize(true)
         observeData()
@@ -64,16 +67,39 @@ class MainActivity : AppCompatActivity(),PlayerAdapter.OnItemClickListener {
 
     fun observeData() {
         viewModel.playerList.observe(this, Observer {
-            Log.i("data", it.toString())
-            println("Test")
-            recycler_view.adapter = PlayerAdapter(viewModel, it, this,this)
+            recycler_view.adapter = PlayerAdapter(viewModel, it, this, this)
         })
     }
 
     override fun onItemClick(position: Int) {
         Toast.makeText(this, "Item $position clicked", Toast.LENGTH_SHORT).show()
-        val clickedItem= viewModel.playerList.value?.player?.get(position)
-        clickedItem?.strPlayer = "Clicked"
+        val clickedItem = viewModel.playerList.value?.player?.get(position)
+        if (clickedItem != null) {
+            viewModel.selectedPlayer.value = clickedItem
+        }
         recycler_view.adapter?.notifyItemChanged(position)
+        toggleView()
     }
+
+    fun backButton(view:View) {
+        toggleView()
+    }
+
+    private fun toggleView(){
+        //List View
+        if(recycler_view.isVisible){
+            recycler_view.visibility = View.GONE
+            searchField.visibility = View.GONE
+            backButton.visibility = View.VISIBLE
+            selectedPlayerView.visibility = View.VISIBLE
+
+        } else{
+            //Selected View
+            recycler_view.visibility = View.VISIBLE
+            searchField.visibility = View.VISIBLE
+            backButton.visibility = View.INVISIBLE
+            selectedPlayerView.visibility = View.GONE
+        }
+    }
+
 }
