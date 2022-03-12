ba5534b3d08c90bbcbba3cf36f47cfd8bf27c5f8
moonryc <70920813+moonryc@users.noreply.github.com>
2022-03-12 16:54:21 -0600

    refactoring

--- app/src/main/java/com/example/hometeam/models/Player.kt
+++ app/src/main/java/com/example/hometeam/models/Player.kt
@@ -1,38 +1,35 @@
 package com.example.hometeam.models
 
-import com.bumptech.glide.Glide
-import com.example.hometeam.R
 
 data class Player(
-    var strPlayer: String = "",
-    val strTeam: String = "",
-    val strNationality: String = "",
-    val strTeam2: String = "",
-    val strSport: String = "",
-    val dateBorn: String = "",
-    val strNumber: String = "",
-    val dateSigned: String = "",
-    val strWage: String = "",
-    val strBirthLocation: String = "",
-    val strDescriptionEN: String = "",
-    val strPosition: String = "",
-    val strHeight: String = "",
-    val strWeight: String = "",
-    val strThumb: String = "",
-    val strCutout: String = "",
-    val strRender: String = "",
-    val strBanner: String = "",
+    var strPlayer: String? = "",
+    val strTeam: String? = "",
+    val strNationality: String? = "",
+    val strTeam2: String? = "",
+    val strSport: String? = "",
+    val dateBorn: String? = "",
+    val strNumber: String? = "",
+    val dateSigned: String? = "",
+    val strWage: String? = "",
+    val strBirthLocation: String? = "",
+    val strDescriptionEN: String? = "",
+    val strPosition: String? = "",
+    val strHeight: String? = "",
+    val strWeight: String? = "",
+    val strThumb: String? = "",
+    val strCutout: String? = "",
+    val strRender: String? = "",
+    val strBanner: String? = "",
+) {
 
-    ){
-
-    fun getImage():String{
-        return if(strThumb !== "" && strThumb !== null){
+    fun getImage(): String {
+        return if (strThumb !== "" && strThumb !== null) {
             strThumb
-        }else if(strCutout !== "" && strCutout !== null){
+        } else if (strCutout !== "" && strCutout !== null) {
             strCutout
-        } else if(strRender !== "" && strRender !== null){
+        } else if (strRender !== "" && strRender !== null) {
             strRender
-        } else{
+        } else {
             ""
         }
     }
